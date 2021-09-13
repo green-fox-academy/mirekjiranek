3bcc0c97429f22b3282edb468e5b6c4fab9ffa54
Miroslav Jiranek <jiranekmiroslav@gmail.com>
2021-09-13 13:32:45 +0200

    Add week-01 excercises

--- day-5/HardOnes/Unique/Program.cs
+++ day-5/HardOnes/Unique/Program.cs
@@ -8,7 +8,6 @@ namespace Unique
         {
             //  Create a method that takes an array of integers as a parameter
             //  and returns an array of integers where every integer is unique (occurs only once)
-
             //  Example
             //Console.WriteLine(FindUniqueItems(new[] { 1, 11, 34, 11, 52, 61, 1, 34 }));
             //  should print: `[1, 11, 34, 52, 61]`

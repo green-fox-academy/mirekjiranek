10670002873808babfdaac8a54abcb2168a46328
Miroslav Jiranek <jiranekmiroslav@gmail.com>
2021-09-13 09:02:30 +0200

    Exercices

---
+++ day-5/HardOnes/Unique/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Unique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Create a method that takes an array of integers as a parameter
+            //  and returns an array of integers where every integer is unique (occurs only once)
+
+            //  Example
+            //Console.WriteLine(FindUniqueItems(new[] { 1, 11, 34, 11, 52, 61, 1, 34 }));
+            //  should print: `[1, 11, 34, 52, 61]`
+
+            }
+        static void FindUniqueItems(int[] arr)
+        {
+            int[] uniqueArr = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+            }
+        }
+        }
+    }

37782a957d95371b5b9a05ad88c64530a0afce59
Miroslav Jiranek <jiranekmiroslav@gmail.com>
2021-10-04 15:20:23 +0200

    Personal Finance completed

--- week-05/Day-1/DataStructure_Practicing/PersonalFinance/Program.cs
+++ week-05/Day-1/DataStructure_Practicing/PersonalFinance/Program.cs
@@ -8,7 +8,7 @@ namespace PersonalFinance
         static void Main(string[] args)
         {
             List<int> PersonalFinance = new List<int> { 500, 1000, 1250, 175, 800, 120 };
-            Console.WriteLine(FinanceInfo(PersonalFinance));
+            FinanceInfo(PersonalFinance);
 
         }
         static string FinanceInfo(List<int> input)
@@ -21,16 +21,16 @@ namespace PersonalFinance
             foreach (int i in input)
             {
                 sum += i;
-                //if (i > greatest)
-                //{
-                //    greatest = i;
-                //}
-                //if (i < cheapest)
-                //{
-                //    cheapest = i;
-                //}
+                if (i > greatest)
+                {
+                    greatest = i;
+                }
+                if (i < cheapest)
+                {
+                    cheapest = i;
+                }
             }
-            return $"cheapest =  {cheapest=input[0]}\nGreatest = {greatest=input[input.Count-1]}\nsum = {sum}\naverage = {sum / input.Count}";
+            return $"cheapest =  {cheapest}\nGreatest = {greatest}\nsum = {sum}\naverage = {sum / input.Count}";
 
         }
     }

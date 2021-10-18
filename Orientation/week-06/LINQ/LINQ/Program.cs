2ecc2fb7569a32c695db8651205263b1d55f63b6
Miroslav Jiranek <jiranekmiroslav@gmail.com>
2021-10-18 11:42:19 +0200

    LINQ Execise 1-3

---
+++ Orientation/week-06/LINQ/LINQ/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = { 1, 3, -2, -4, -7, -3, -8, 12, 19, 6, 9, 10, 14 };
+            Exercise3(n);
+        }
+        static public int[] Exercise1(int[] input)
+        {
+            //Write a LINQ Expression to get the even numbers from the following array:
+            int[] output =  input.Where(input => input % 2 == 0).ToArray();
+            return output;
+        }
+        static public double Exercise2(int[] input)
+        {
+            //Write a LINQ Expression to get the average value of the odd numbers from the following array:
+            var output = input.Where(input => input % 2 != 0).Average();
+            return output;
+        }
+        static public List<int> Exercise3(int[] input)
+        {
+            //Write a LINQ Expression to get the squared value of the positive numbers from the following array:
+            var output = input.Where(input => input > 0).Select(input => input * input).ToList();
+            return output;
+
+        }
+    }
+}
